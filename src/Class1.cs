990e94bea31a3d4a60ab69bc0c7d9d9fdab5b620
Guillaume Doré <gdore@escardio.org>
2024-09-02 01:40:47 +0200

    Add segmented button source

--- src/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UiToolkit.Maui.Controls;
-
-// All the code in this file is included in all platforms.
-public class Class1
-{
-}
---
+++ src/SegmentedButton.xaml.cs
@@ -0,0 +1,47 @@
+using Maui.BindableProperty.Generator.Core;
+
+#pragma warning disable IDE0052, CS0169
+namespace UiToolkit.Maui.Controls;
+
+public partial class SegmentedButton : ContentView
+{
+	[AutoBindable]
+	private readonly string? _fontFamily;
+
+	[AutoBindable(DefaultValue = "true")]
+	private readonly bool _isEnabled;
+
+	[AutoBindable]
+	private readonly Brush? _stroke;
+
+	[AutoBindable]
+	private readonly double _strokeThickness;
+
+	[AutoBindable]
+	private readonly Color? _selectedColor;
+
+	[AutoBindable]
+	private readonly Color? _selectedBackground;
+
+	[AutoBindable]
+	private readonly Color? _unselectedColor;
+
+	[AutoBindable]
+	private readonly Color? _unselectedBackground;
+
+	[AutoBindable]
+	private readonly string[]? _segmentItems;
+
+	[AutoBindable(DefaultBindingMode = "TwoWay")]
+	private readonly string? _selectedItem;
+
+	public SegmentedButton()
+	{
+		InitializeComponent();
+	}
+
+	public string GroupName => Guid.NewGuid().ToString();
+
+	private void SegmentItem_Clicked(object sender, EventArgs e)
+		=> SelectedItem = ((Button)sender).Text;
+}
