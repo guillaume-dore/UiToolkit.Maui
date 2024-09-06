e86854b45684f5b5ff5f8f156dd292fbb95ea3be
Guillaume Doré <gdore@escardio.org>
2024-09-06 10:47:40 +0200

    add conditional view control

---
+++ src/ConditionalView.cs
@@ -0,0 +1,17 @@
+﻿using Maui.BindableProperty.Generator.Core;
+
+namespace UiToolkit.Maui.Controls;
+
+public partial class ConditionalView : ContentView
+{
+	[AutoBindable(OnChanged = nameof(UpdateContent))]
+	private readonly bool _condition;
+
+	[AutoBindable(OnChanged = nameof(UpdateContent))]
+	private readonly View? _true;
+
+	[AutoBindable(OnChanged = nameof(UpdateContent))]
+	private readonly View? _false;
+
+	private void UpdateContent() => Content = Condition ? True : False;
+}
