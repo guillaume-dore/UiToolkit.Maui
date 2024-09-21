f684eb767d2f90a72fedef03c970b02413679046
Guillaume Doré <guillaumedore.76@hotmail.fr>
2024-09-21 18:22:41 +0200

    Add EnumToString Bidirectionnal converter.

---
+++ src/Converters/EnumToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace UiToolkit.Maui.Converters;
+
+public class EnumToStringConverter : IValueConverter
+{
+	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		=> value != null ? System.Convert.ChangeType(value, value!.GetType()).ToString() : null;
+
+	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		Type type = targetType;
+		if (targetType.IsGenericType && targetType.GetGenericTypeDefinition() == typeof(Nullable<>))
+			type = Nullable.GetUnderlyingType(targetType)!;
+		if (Enum.TryParse(type, (string)value!, out var result))
+			return result;
+		else
+			return null;
+	}
+}
